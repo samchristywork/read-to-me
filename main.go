51f14780a4849e90b7ba80405091c94ca810a438
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-09-10 21:35:44 -0500

    feat: Add function for synthesizing speech
    This function uses a TTS library to make an audio file that can be
    replayed at will.

--- main.go
+++ main.go
@@ -10,6 +10,64 @@ import (
 	"os"
 )
 
+func fileExists(filename string) bool {
+	_, err := os.Stat(filename)
+	return !os.IsNotExist(err)
+}
+
+func tts(inputText string, shaSum string) error {
+	textFilename := fmt.Sprintf("data/text-%s.txt", shaSum)
+	outputFilename := fmt.Sprintf("data/output-%s.mp3", shaSum)
+
+	if fileExists(outputFilename) {
+		return nil
+	}
+
+	ctx := context.Background()
+
+	client, err := texttospeech.NewClient(ctx)
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	req := texttospeechpb.SynthesizeSpeechRequest{
+		Input: &texttospeechpb.SynthesisInput{
+			InputSource: &texttospeechpb.SynthesisInput_Text{Text: string(inputText)},
+		},
+
+		Voice: &texttospeechpb.VoiceSelectionParams{
+			LanguageCode: "en-US",
+			SsmlGender:   texttospeechpb.SsmlVoiceGender_MALE,
+		},
+
+		AudioConfig: &texttospeechpb.AudioConfig{
+			AudioEncoding: texttospeechpb.AudioEncoding_MP3,
+		},
+	}
+
+	resp, err := client.SynthesizeSpeech(ctx, &req)
+	if err != nil {
+		return err
+	}
+
+	textFile, err := os.Create(textFilename)
+	if err != nil {
+		return err
+	}
+	defer textFile.Close()
+	textFile.Write([]byte(inputText))
+
+	outFile, err := os.Create(outputFilename)
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+	outFile.Write(resp.AudioContent)
+
+	return nil
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "index.html")
