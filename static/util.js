dcfe24f1228dfcfd76f53628df4285e7e3fdac94
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-10-03 21:37:24 -0500

    feat: Add JS to handle cookies
    This code handles whether the cookie banner is displayed. Right now it
    is only on the "play" page, but that will soon change.

--- static/util.js
+++ static/util.js
@@ -12,3 +12,16 @@ function getCookie(cname) {
   }
   return "";
 }
+
+function acceptCookies() {
+  document.cookie = "cookieConsent=true; path=/; max-age=" + 60 * 60 * 24 * 365;
+  document.getElementById('cookie-banner').style.display = 'none';
+}
+
+window.onload = function() {
+  if (document.cookie.indexOf("cookieConsent=true") === -1) {
+    document.getElementById('cookie-banner').style.display = 'block';
+  } else {
+    document.getElementById('cookie-banner').style.display = 'none';
+  }
+}
