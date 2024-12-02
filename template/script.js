635f3a8a7b57209b3b4fa0e00790f335295e2e4f
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-12-01 20:41:19 -0600

    feat: Add script for scrolling audio content
    This script automatically scrolls the document while the audio player is
    playing. The text is also highlighted to show where the playback is
    currently.

---
+++ template/script.js
@@ -0,0 +1,39 @@
+<script>
+  document.addEventListener('DOMContentLoaded', function() {
+    const audioPlayer = document.querySelector('#audioPlayer');
+    const postBody = document.querySelector('.post-body');
+    const textBlocks = postBody.querySelectorAll('div');
+    const timingSpans = postBody.querySelectorAll('span.hidden');
+    const autoscroll = document.querySelector('#autoscroll')
+
+    let blockDurations = [];
+    timingSpans.forEach(span => {
+      blockDurations.push(parseInt(span.textContent, 10));
+    });
+
+    function updateTextStyles() {
+      let currentTime = audioPlayer.currentTime * 1000;
+      let accumulatedTime = 0;
+
+      let first = true;
+      textBlocks.forEach((block, index) => {
+        accumulatedTime += blockDurations[index];
+
+        if (currentTime >= accumulatedTime) {
+          block.style.color = 'grey';
+        } else {
+          block.style.color = 'black';
+          if (first == true) {
+            if (autoscroll.checked) {
+              let y = block.offsetTop-70;
+              window.scrollTo({top: y, behavior: 'smooth'});
+            }
+            first = false;
+          }
+        }
+      });
+    }
+
+    audioPlayer.addEventListener('timeupdate', updateTextStyles);
+  });
+</script>`
