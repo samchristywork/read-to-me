d1ffdd309759b7ee2f9aafa83fe8e290b1a9ec27
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-12-01 20:19:23 -0600

    feat: Add audio player to post page
    This audio resource plays the audio of a given post. Right now, the
    `/audio` endpoint is not implemented.

--- main.go
+++ main.go
@@ -163,6 +163,20 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	audio := fmt.Sprintf(`<div class="audio-container">
+	<audio id="audioPlayer" controls="controls" autobuffer="autobuffer">
+		<source src="/audio?id=%s&foo=bar" />
+	</audio>
+	<div>
+		<button onclick="document.getElementById('audioPlayer').playbackRate = 0.5;">0.5x</button>
+		<button onclick="document.getElementById('audioPlayer').playbackRate = 1;">1x</button>
+		<button onclick="document.getElementById('audioPlayer').playbackRate = 1.5;">1.5x</button>
+		<button onclick="document.getElementById('audioPlayer').playbackRate = 2;">2x</button>
+	</div>
+	<label for="autoscroll">Autoscroll:</label>
+	<input id="autoscroll" name="autoscroll" type=checkbox checked></input>
+	</div>`, id)
+
 	content := ""
 	lines := strings.Split(body, "\n")
 
@@ -198,7 +212,13 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Printf("Total length of all lines (in ms): %d\n", totalLength)
 
-	content = `<main>` + string(post(id, title, source, content, author, timestamp, "full")) + `</main>`
+	script, err := templateFiles.ReadFile("template/script.js")
+	if err != nil {
+		http.Error(w, "Internal Server Error: Unable to load page", http.StatusInternalServerError)
+		return
+	}
+
+	content = `<main>` + string(post(id, title, source, content, author, timestamp, "full")) + audio + `</main>` + string(script)
 
 	page, err := renderPage(content)
 	if err != nil {
