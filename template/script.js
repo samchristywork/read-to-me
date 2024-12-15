8d1468ba7e08a4f266f0e7cf00a424bc304e5fd1
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-12-14 19:16:30 -0600

    feat: Change autoscroll to be in relative units
    Autoscroll used to be in pixels, but now it is 10rem, which is a
    relative value.

--- template/script.js
+++ template/script.js
@@ -5,12 +5,16 @@
     const textBlocks = postBody.querySelectorAll('div');
     const timingSpans = postBody.querySelectorAll('span.hidden');
     const navigation = document.querySelectorAll('.navigation div');
-    const autoscroll = document.querySelector('#autoscroll')
+    const autoscroll = document.querySelector('#autoscroll');
 
     let blockDurations = [];
     timingSpans.forEach(span => {
       blockDurations.push(parseInt(span.textContent, 10));
     });
+
+    const s = parseFloat(getComputedStyle(document.documentElement).fontSize);
+    const offsetInRem = 10;
+    const offsetInPixels = offsetInRem * s;
 
     function updateTextStyles() {
       let currentTime = audioPlayer.currentTime * 1000;
@@ -22,18 +26,18 @@
 
         if (currentTime >= accumulatedTime) {
           block.style.color = 'grey';
-          navigation[index].style.color='grey';
+          navigation[index].style.color = 'grey';
         } else {
           block.style.color = 'black';
           if (first == true) {
             if (autoscroll.checked) {
-              let y = block.offsetTop-100;
-              window.scrollTo({top: y, behavior: 'smooth'});
+              let y = block.offsetTop - offsetInPixels;
+              window.scrollTo({ top: y, behavior: 'smooth' });
             }
-            navigation[index].style.color='black';
+            navigation[index].style.color = 'black';
             first = false;
           } else {
-            navigation[index].style.color='grey';
+            navigation[index].style.color = 'grey';
           }
         }
       });
@@ -41,4 +45,4 @@
 
     audioPlayer.addEventListener('timeupdate', updateTextStyles);
   });
-</script>`
+</script>
